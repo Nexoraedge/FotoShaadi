7f9a735c363aac09bda2499c18c32c3fa762255d
Nexoraedge <onlyego1043@gmail.com>
2025-11-17 13:30:47 +0530

    Navbar

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+const Hero = () => {
+  return (
+   <section className=" bg-black justify-center">
+    
+</section>
+
+
+
+  )
+}
+
+export default Hero
---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+"use client"
+import Image from 'next/image'
+import { useState } from 'react'
+import { Button } from './ui/button'
+
+const Navbar = () => {
+    const [login, setLogin] = useState(true)
+
+    return (
+        <div className='bg-[#EEE6E7] p-2 px-5 h-16 w-full'>
+            <div className="center w-full  h-full  flex justify-between">
+                
+                <div className="flex items-center gap-2">
+                    <Image src="/assets/Logo_Png-removebg-preview.png" alt=" LoGo" width={200} height={200} className='w-[48] ' />
+                </div>
+
+                {!login ? <div className="flex items-center">
+                    <Button className='px-4 h-[80%]  font-[Lexen] text-lg xl:px-6 xl:h-[85%]  xl:font-bold border-2 border-[#d6b289] hover:bg-[#c36c74] text-center flex justify-center bg-[#5A0E15] text-white/85 transition-colors duration-200 ease-linear hover:cursor-pointer hover:text-[#5A0E15] '>Login</Button>
+                </div> : <div className="flex items-center ">
+                    <Button className='p-0'><Image src="/assets/Menu.svg" alt=" LoGo" width={100} height={100} className='w-[30] cursor-pointer' /></Button>
+                </div>}
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
